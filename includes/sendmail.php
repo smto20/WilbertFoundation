f106141e4c77cd08851814d7912ccaef0190b46b
webdeveloperpnkj <panku63207@gmail.com>
2017-03-02 01:32:42 +0530

    Add files via upload

--- includes/sendmail.php
+++ includes/sendmail.php
@@ -1,78 +1,41 @@
 <?php
 
-require_once('phpmailer/class.phpmailer.php');
-require_once('phpmailer/class.smtp.php');
+if(isset($_POST['submit']) && $_POST['submit'] == 'submitForm'){
+	
+	$to = 'panku63207@gmail.com'; // Your Email Address
+	
+	$name = $_POST['form_name'];
+	$email = $_POST['form_email'];
+	$inquiry = $_POST['form_subject'];
+	$phone = $_POST['form_phone'];
+	$message = $_POST['form_message'];
+	
+	$name = isset($name) ? "Name: $name<br><br>" : '';
+	$email = isset($email) ? "Email: $email<br><br>" : '';
+	$phone = isset($phone) ? "Phone: $phone<br><br>" : '';
+	$inquiry = isset($inquiry) ? "Website Inquiry: $inquiry<br><br>" : '';
+	$message = isset($message) ? "Message: $message<br><br>" : '';
 
-$mail = new PHPMailer();
+	$referrer = $_SERVER['HTTP_REFERER'] ? '<br><br><br>This Form was submitted from: ' . $_SERVER['HTTP_REFERER'] : '';
+	
+	$subject = 'Contact form from Website';
+	$body = "$name $email $phone $inquiry $message $referrer";
+	
+	$headers = '';
+	$headers = 'MIME-Version: 1.0'.PHP_EOL;
+	$headers .= 'Content-type: text/html; charset=iso-8859-1'.PHP_EOL;
+	//$headers .= 'From: webmaster@example.com<From: webmaster@example.com>'.PHP_EOL; 
+	
+	$sendEmail = mail($to, $subject, $body, $headers);
 
-
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'just55.justhost.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'themeforest@ismail-hossain.me';                 // SMTP username
-$mail->Password = 'AsDf12**';                           // SMTP password
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465;                                    // TCP port to connect to
-
-$message = "";
-$status = "false";
-
-if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
-    if( $_POST['form_name'] != '' AND $_POST['form_email'] != '' AND $_POST['form_subject'] != '' ) {
-
-        $name = $_POST['form_name'];
-        $email = $_POST['form_email'];
-        $subject = $_POST['form_subject'];
-        $phone = $_POST['form_phone'];
-        $message = $_POST['form_message'];
-
-        $subject = isset($subject) ? $subject : 'New Message | Contact Form';
-
-        $botcheck = $_POST['form_botcheck'];
-
-        $toemail = 'spam.thememascot@gmail.com'; // Your Email Address
-        $toname = 'ThemeMascot'; // Your Name
-
-        if( $botcheck == '' ) {
-
-            $mail->SetFrom( $email , $name );
-            $mail->AddReplyTo( $email , $name );
-            $mail->AddAddress( $toemail , $toname );
-            $mail->Subject = $subject;
-
-            $name = isset($name) ? "Name: $name<br><br>" : '';
-            $email = isset($email) ? "Email: $email<br><br>" : '';
-            $phone = isset($phone) ? "Phone: $phone<br><br>" : '';
-            $message = isset($message) ? "Message: $message<br><br>" : '';
-
-            $referrer = $_SERVER['HTTP_REFERER'] ? '<br><br><br>This Form was submitted from: ' . $_SERVER['HTTP_REFERER'] : '';
-
-            $body = "$name $email $phone $message $referrer";
-
-            $mail->MsgHTML( $body );
-            $sendEmail = $mail->Send();
-
-            if( $sendEmail == true ):
-                $message = 'We have <strong>successfully</strong> received your Message and will get Back to you as soon as possible.';
-                $status = "true";
-            else:
-                $message = 'Email <strong>could not</strong> be sent due to some Unexpected Error. Please Try Again later.<br /><br /><strong>Reason:</strong><br />' . $mail->ErrorInfo . '';
-                $status = "false";
-            endif;
-        } else {
-            $message = 'Bot <strong>Detected</strong>.! Clean yourself Botster.!';
-            $status = "false";
-        }
-    } else {
-        $message = 'Please <strong>Fill up</strong> all the Fields and Try Again.';
-        $status = "false";
-    }
-} else {
-    $message = 'An <strong>unexpected error</strong> occured. Please Try Again later.';
-    $status = "false";
+	if( $sendEmail){
+		$message = 'We have <strong>successfully</strong> received your Message and will get Back to you as soon as possible.';
+		$status = "true";
+	}else{
+		$message = 'Email <strong>could not</strong> be sent due to some Unexpected Error. Please Try Again later.<br /><br />';
+		$status = "false";
+	}    
+	$status_array = array( 'message' => $message, 'status' => $status);
+	echo json_encode($status_array);	
 }
-
-$status_array = array( 'message' => $message, 'status' => $status);
-echo json_encode($status_array);
 ?>
